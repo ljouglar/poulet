2dd8f67128d4b3392326256019d8a7e231d2c811
Laurent Jouglar <ljouglar@ipneos.com>
2024-09-23 23:53:47 +0200

    build: :rocket: test deploy on new server

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App.tsx';
 
+// Commentaire pour forcer le push
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
